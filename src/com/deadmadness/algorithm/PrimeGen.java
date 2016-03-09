c3f845ad1a0780e84b46a0d648eebe40c18d2f74
Jamal <bluedocjamal@hotmail.co.uk>
2016-03-09 12:33:13 +0000

    refined and generating keys

--- src/com/deadmadness/algorithm/PrimeGen.java
+++ src/com/deadmadness/algorithm/PrimeGen.java
@@ -13,20 +13,40 @@ public class PrimeGen {
 	private SecureRandom r = new SecureRandom();
 	
 	//Set to 128 for 2^128 numbers
-	private static int bitSize = 60;
+	private static int bitSize = 32;
 	private static final BigInteger TWO = new BigInteger("2");
 
 	private BigInteger p;
+	private BigInteger q;
+	private BigInteger n;
 	private BigInteger e;
+	private BigInteger d;
+	private BigInteger phi;
 	
 	PrimeGen(){
 		//constructor
+		p = genLargePrime();
+		q = genLargePrime();
+		n = p.multiply(q);
+				
+		// phi = (p-1) * (q-1)
+		phi = (p.subtract(BigInteger.ONE).multiply((q.subtract(BigInteger.ONE))));
+		e = genE(phi);
+		
+		
+		while(e.equals(BigInteger.ZERO)){
+			e = genE(phi);
+		}
+		
+		d = modInverse(phi, e);
+
 	}
 
 	
 	//generates a prime number in a large  num range
 	public BigInteger genLargePrime() {
 		r = new SecureRandom();
+		BigInteger p;
 	
 		while(true){
 			p = new BigInteger(bitSize, r); //p = r.nextInt(Integer.MAX_VALUE/5 - (Integer.MAX_VALUE/10)) + (Integer.MAX_VALUE/10);
@@ -37,7 +57,9 @@ public class PrimeGen {
 				System.out.println("Prime Number:" + p);
 				break;
 			}
-			System.out.println("no prime found");
+			else {
+				System.out.println("no prime found");
+			}
 		}
 		return p;
 	}
@@ -53,58 +75,53 @@ public class PrimeGen {
 			}
 		}
 		return e;
-	}
+	}*/
 	
-	public boolean relPrime(int e, int n) {
-		if(gcd(e,n) == 1) {
-			System.out.println("Done!");
-			return true;
+	public BigInteger genE(BigInteger phi) {
+		BigInteger e = genLargePrime();
+		
+		if(e.compareTo(phi) == 1){
+			System.out.println("E is larger than Phi - failed!");
+			return BigInteger.ZERO;
+		}
+		
+		if(gcd(e,phi)) {
+			System.out.println("Relatively prime: "+e+" "+phi);
+			return e;
 		}
 		else{
-			System.out.println("Failed, not relatively prime");
-			return false;
+			System.out.println("Failed, E and phi not relatively prime");
+			return BigInteger.ZERO;
 		}
 	}
-	/*
-	//calculates gcd
-	private int gcd(int a, int b) {
-		int n = 0;
-		int i;
-		if(b>a) {
-			if(b%a == 0) {
-				n = b/a;
-				return n;
-			}
-			else {
-				i = b%a;
-			}
-			while(i != 0) {
-				b = a;
-				a = i;
-				i = b%a;
-			}
-			n = a;
-			return n;
-		}
-		else if(a>b) {
-			if(a%b == 0) {
-				n = a/b;
-				return n;
-			}
-			else {
-				i = a%b;
-			}
-			while(i != 0) {
-				a = b;
-				b = i;
-				i = a%b;
-			}
-			n = b;
-			return n;
-		}
-		return n;
+
+	
+	
+	//calculates if e and phi are relatively prime
+	private boolean gcd(BigInteger a, BigInteger b) {
+		BigInteger i;
 		
-	}*/
+		if(b.mod(a) == BigInteger.ZERO) {
+			return false;
+		}
+		else {
+			i = b.mod(a);
+		}
+		while(i.compareTo(BigInteger.ZERO) != 0) {
+			b = a;
+			a = i;
+			i = b.mod(a);
+		}
+		if(a.compareTo(BigInteger.ONE) == 0){
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
+	
+	
 	
 	// checks to see if prime number
 	private boolean isPrime(BigInteger n) {
@@ -128,11 +145,10 @@ public class PrimeGen {
 		 */
 		for(BigInteger i = new BigInteger("3"); i.compareTo(sqRoot)==-1; i = i.add(TWO)) 
 		{
-			//System.out.println(i);
 			//check if num divides evenly anywhere
 			if(n.remainder(i).equals(BigInteger.ZERO)) {
 				
-				System.out.println("Not prime!");
+				System.out.println("Not prime at number: " + i);
 				return false;
 			}
 			
@@ -140,14 +156,60 @@ public class PrimeGen {
 		return true;
 	}
 	
+	
+	//generates an approx root of prime number
 	private static BigInteger root(BigInteger n) {
 		BigInteger divisor = n.shiftRight(1);
 		//while ((n/2) * (n/2)) greater than N, 
 		while(divisor.multiply(divisor).compareTo(n) > 0) {
 			divisor = divisor.shiftRight(1);
 		}
+		//System.out.println("Root: " + divisor.shiftLeft(1));
 		return divisor.shiftLeft(1);
 	}
 	
+	
+	//Gets ed mod phi = 1
+	public BigInteger modInverse(BigInteger phi ,BigInteger e){
+		
+		BigInteger p = phi;
+		BigInteger x1 = BigInteger.ZERO;
+		BigInteger x2 = BigInteger.ONE;
+		BigInteger y1 = BigInteger.ONE;
+		BigInteger y2 = BigInteger.ZERO;
+		BigInteger temp;
+		BigInteger q, r;
+		
+		while(!e.equals(BigInteger.ZERO)){
+			q = p.divide(e);
+			r = p.subtract(q.multiply(e));
+			
+			p = e;
+			e = r;
+			
+			temp = x1;
+			x1 = x2.subtract(q.multiply(x1));
+			x2 = temp;
+			
+			temp = y1;
+			y1 = y2.subtract(q.multiply(y1));
+			y2 = temp;
+		}
+		if(y2.compareTo(BigInteger.ZERO) == -1){
+			return phi.add(y2);
+		}
+		else{
+			return y2;
+		}
+	}
 
+
+	//returns generated keys
+	public BigInteger[] getKeys(){
+		BigInteger[] keys = new BigInteger[3];
+		keys[0] = e;
+		keys[1] = n;
+		keys[2] = d;
+		return keys;
+	}
 }
--- src/com/deadmadness/algorithm/RSA.java
+++ src/com/deadmadness/algorithm/RSA.java
@@ -10,44 +10,20 @@ import java.math.BigInteger;
  */
 public class RSA {
 
-	/**
-	 * @param args
-	 */
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PrimeGen generator = new PrimeGen();
-		BigInteger p = generator.genLargePrime();
-		BigInteger q = generator.genLargePrime();
-		BigInteger n = p.multiply(q);
+		BigInteger[] keys = new BigInteger[3];
+		
+		keys = generator.getKeys();
 		
 		System.out.println("Done!!");
-		System.out.println("P = " + p);
-		System.out.println("Q = " + q);
-		System.out.println("N = " + n);
-		
-		// phi = (p-1) * (q-1)
-		BigInteger phi = (p.subtract(BigInteger.ONE).multiply((q.subtract(BigInteger.ONE))));
-		
-		BigInteger pubKey;
+		System.out.println("E = " + keys[0]);
+		System.out.println("N = " + keys[1]);
+		System.out.println("D = " + keys[2]);
 
-		System.out.println("Phi:" + phi);
-		/*
-		pubKey = generator.genPublicKey(phi);
-		*/
-		/*
-		e = generator.genPrime();
 		
-		System.out.println("P:" + p + " Q:" + q + " N minus: " + nMinus);
-		
-		if(generator.relPrime(e,nMinus)){
-			pubKey = "" + n +", "+ e;
-			System.out.println("Public Key:" + pubKey);
-		}
-		else {
-			System.out.println("No relPrime found");
-		}
-		*/
+
 	}
 
 }
